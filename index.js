89cca75c6525b277a8a7945ca48acce66c5f79ca
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-13 19:51:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,7 @@ function nameToAttributes(drivers) {
 }
 
 function attributesToPhrase(drivers) {
-  
+  return drivers.map(function(driver){
+    return '${driver.name} is from ${driver.hometown}'
+  })
 }
