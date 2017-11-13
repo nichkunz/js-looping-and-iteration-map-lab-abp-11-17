fc6af82e551d4525cac4b91fc2faa7c1a269e88d
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-13 19:49:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,29 +13,6 @@ function nameToAttributes(drivers) {
   })
 }
 
-// function nameToAttributes(drivers){
-// //make new array with 2 object attributes, firstName and lastName
-//   return drivers.map(function (driver){
-//     driver.split(" ");
-//   })
-// }
-
-
-//
-// const nameToAttributes = drivers.map(function(driver) {
-//   let newDrivers = drivers.split(" ");
-//   return Object.assign({}, newDrivers, { firstName: newDrivers[0]})
-//   return Object.assign({}, newDrivers, { lastName: newDrivers[1]})
-// })
-
-
-// function nameToAttributes(drivers){
-//   return drivers.map(function (driver){
-//     driver.split(" ");
-//       Object.assign({}, driver, {driver[0] : driver[1]};
-//   })
-// }
-//
-// function nameToAttributes(drivers) {
-//
-// }
+function attributesToPhrase(drivers) {
+  
+}
