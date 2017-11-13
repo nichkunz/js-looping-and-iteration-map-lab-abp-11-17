ddca4b1a804e87655aef60d926ca5a84d375f310
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-13 19:44:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function lowerCaseDrivers(drivers){
 
 function nameToAttributes(drivers) {
   return drivers.map(function(driver){
-    const driverFName = driver.split(" ")[0];
-    const driverLName = driver.split(" "[1]);
+    const driverFName = driver.split(' ')[0];
+    const driverLName = driver.split(' '[1]);
     return { firstName: driverFName, lastName: driverLName };
   })
 }
